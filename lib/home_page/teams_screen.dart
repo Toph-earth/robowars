fd37efa98e076595208b1c7be8e57b059a37f5c8
Jacinth <jacinth@Jacinths-MacBook-Air.local>
2025-08-02 08:27:26 +0530

    Updated UI

--- lib/home_page/teams_screen.dart
+++ lib/home_page/teams_screen.dart
@@ -260,7 +260,17 @@ class _TeamScreenState extends State<TeamScreen>  {
         Container(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
           decoration: BoxDecoration(
-            border: Border.all(color: Color(0xFF9D3AE7), width: 1.5),
+            border: GradientBoxBorder(
+              width: 3,
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(250, 96, 60, 147),
+                  Color.fromARGB(255, 93, 62, 137),
+                  Color.fromRGBO(119, 95, 154, 0.98),
+                  Color.fromARGB(255, 161, 146, 186),
+                ],
+              ),
+            ),
             borderRadius: BorderRadius.circular(20),
           ),
           child: ListView.separated(
@@ -379,8 +389,18 @@ class _TeamScreenState extends State<TeamScreen>  {
         return Container(
           margin: const EdgeInsets.only(bottom: 16),
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            border: Border.all(color: const Color(0xFF9D3AE7), width: 1.5),
+            border: GradientBoxBorder(
+              width: 2,
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(250, 96, 60, 147),
+                  Color.fromARGB(255, 93, 62, 137),
+                  Color.fromRGBO(119, 95, 154, 0.98),
+                  Color.fromARGB(255, 161, 146, 186),
+                ],
+              ),
+            ),
+            borderRadius: BorderRadius.circular(10),
           ),
           child: Row(
             children: [
